ef437e0cc17415a2fd6d5da7ecfbb451f2a6a3aa
Filip Nedelkovski <filip.nedelkovski@webfactory.mk>
2019-07-31 18:04:50 +0200

    added test for the model

---
+++ src/test/java/com/example/mavenDemoRest/model/LocationTest.java
@@ -0,0 +1,44 @@
+package com.example.mavenDemoRest.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LocationTest {
+
+    Location location;
+
+    @Before
+    public void setUp(){
+        location = new Location();
+    }
+
+    @Test
+    public void getCity() {
+        String city = "Paris";
+        location.setCity(city);
+        assertEquals(city, location.getCity());
+    }
+
+    @Test
+    public void getCountry() {
+        String country = "Andora";
+        location.setCountry(country);
+        assertEquals(country, location.getCountry());
+    }
+
+    /*@Test
+    public void getLongitude() {
+        float lon = 23.3f;
+        location.setLongitude(lon);
+        assertEquals(lon, location.getLongitude());
+    }
+
+    @Test
+    public void getLatitude() {
+        float lat = 44.5f;
+        location.setLatitude(lat);
+        assertEquals(lat, location.getLatitude());
+    }*/
+}
---
+++ src/test/java/com/example/mavenDemoRest/model/PostTest.java
@@ -0,0 +1,46 @@
+package com.example.mavenDemoRest.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PostTest {
+
+    Post post;
+
+    @Before
+    public void setUp(){
+        post = new Post();
+    }
+
+    @Test
+    public void getTitle() {
+        String title = "Sunshine";
+        post.setTitle(title);
+        assertEquals(title, post.getTitle());
+    }
+
+    @Test
+    public void getDescription() {
+        String description = "At vero eos et accusamus et iusto odio dignissimos ducimus";
+        post.setDescription(description);
+        assertEquals(description, post.getDescription());
+    }
+
+    @Test
+    public void getUser() {
+        User user = new User();
+        user.setId(1L);
+        post.setUser(user);
+        assertEquals(user.getId(), post.getUser().getId());
+    }
+
+    @Test
+    public void getLocation() {
+        Location location = new Location();
+        location.setId(1L);
+        post.setLocation(location);
+        assertEquals(location.getId(), post.getLocation().getId());
+    }
+}
