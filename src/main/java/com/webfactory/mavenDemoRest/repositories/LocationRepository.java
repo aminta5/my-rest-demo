24092e157547b6f2606929986bbde2dc17293de5
Filip Nedelkovski <filip.nedelkovski@webfactory.mk>
2019-09-23 09:36:10 +0200

    #31 changed location repository

--- src/main/java/com/webfactory/mavenDemoRest/repositories/LocationRepository.java
+++ src/main/java/com/webfactory/mavenDemoRest/repositories/LocationRepository.java
@@ -2,6 +2,7 @@ package com.webfactory.mavenDemoRest.repositories;
 
 import com.webfactory.mavenDemoRest.model.Location;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.Optional;
 
