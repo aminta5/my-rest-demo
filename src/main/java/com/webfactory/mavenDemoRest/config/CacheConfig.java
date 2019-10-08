041f00548e1b0589ee3efec917deb09e1cc781c6
Filip Nedelkovski <filip.nedelkovski@webfactory.mk>
2019-10-08 17:59:09 +0200

    small changes gradle and cacheConfig

--- src/main/java/com/webfactory/mavenDemoRest/config/CacheConfig.java
+++ src/main/java/com/webfactory/mavenDemoRest/config/CacheConfig.java
@@ -1,10 +1,8 @@
 package com.webfactory.mavenDemoRest.config;
 
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
